d5c706088de3961967cf7579b3bf705634d26efd
Lane Winham <mlwinham12@gmail.com>
2016-09-12 09:21:44 -0600

    Add feature test for user story #1

---
+++ spec/features/guest_searches_with_zip_code_spec.rb
@@ -0,0 +1,21 @@
+require "rails_helper"
+
+RSpec.feature "Guest searches with a zip-code" do
+  scenario "a guest enters a zip-code and is show the ten closest" do
+    visit "/"
+
+    expect(page).to have_content "Zip-code"
+    expect(page).to have_button "Locate"
+
+    fill_in "Zip-code", with: 80215
+    click_button "Locate"
+  end
+end
+# As a user
+# When I visit "/"
+# And I fill in the search form with 80203
+# And I click "Locate"
+# Then I should be on page "/search" with parameters visible in the url
+# Then I should see a list of the 10 closest stations within 6 miles sorted by distance
+# And the stations should be limited to Electric and Propane
+# And for each of the stations I should see Name, Address, Fuel Types, Distance, and Access Times
